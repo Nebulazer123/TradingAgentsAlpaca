e0da2d7035d845b9fda462c5bce35b02d04902a7
Corbin Floyd <nebulazer2003@gmail.com>
2026-07-19 12:25:38 -0400

    feat: compile genomes to inert paper decisions

--- 01_REPO/tradingagents-main/tradingagents/strategy/__init__.py
+++ 01_REPO/tradingagents-main/tradingagents/strategy/__init__.py
@@ -1,5 +1,13 @@
 """Inert, data-only strategy definitions."""
 
+from .compiler import (
+    PAPER_DECISION_SCHEMA_VERSION,
+    GenomePaperDecision,
+    PaperCandidateState,
+    PaperDecisionAction,
+    StrategyObservation,
+    compile_genome_paper_decision,
+)
 from .genome import (
     STRATEGY_EVOLUTION_POLICY_SCHEMA_VERSION,
     STRATEGY_GENOME_SCHEMA_VERSION,
@@ -18,18 +26,24 @@ from .genome import (
 )
 
 __all__ = [
+    "PAPER_DECISION_SCHEMA_VERSION",
     "STRATEGY_EVOLUTION_POLICY_SCHEMA_VERSION",
     "STRATEGY_GENOME_SCHEMA_VERSION",
     "CatalystRelativeStrengthMutationBounds",
     "CatalystRelativeStrengthParameters",
     "CurrentAggressiveMutationBounds",
     "CurrentAggressiveParameters",
+    "GenomePaperDecision",
     "HoldCashParameters",
+    "PaperCandidateState",
+    "PaperDecisionAction",
     "PullbackSupportMutationBounds",
     "PullbackSupportParameters",
     "StrategyEvolutionPolicy",
     "StrategyFamily",
     "StrategyGenome",
     "StrategyMutationBounds",
+    "StrategyObservation",
+    "compile_genome_paper_decision",
     "load_strategy_evolution_policy",
 ]
